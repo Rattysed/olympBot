7e7bc8c09b5520b253bf1ebacfb4772fd62ec183
Rattysed <ckvortcov71@gmail.com>
2022-08-01 00:21:38 +0300

    Добавил запуск сервера из main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+
+
+
+def run_server(state=0):
+    if state:
+        os.system('python manage.py migrate')
+    os.system('python manage.py runserver')
+
+
+if __name__ == '__main__':
+    state = input('Выполнить миграции? Y/N').lower()
+    if state == 'y':
+        state = 1
+    else:
+        state = 0
+    run_server(state)
