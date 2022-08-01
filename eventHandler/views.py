92a50d1ae7b5fd0cbff49887d99569e45c24597a
Andrew <zhogov.andrey.d@gmail.com>
2022-08-01 13:16:12 +0300

    тест коммит

--- eventHandler/views.py
+++ eventHandler/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from eventHandler.request_handler import get_all_this_date
 from datetime import date
 
+
 def test(request):
     return HttpResponse(f"<h2>{get_all_this_date(date(2022, 8, 2))[0].name}</h2>")
 
