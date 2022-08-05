402b0c4f200c91e97acbd891eb115288c3638051
Rattysed <ckvortcov71@gmail.com>
2022-08-05 14:13:19 +0300

    Фикс ошибки валидации

--- eventHandler/forms.py
+++ eventHandler/forms.py
@@ -1,18 +1,26 @@
 from django import forms
+from .models import Subjects, Events, Profiles
+
+blank_choice = (('', '--- Выберите значение ---'),)
+EVENT_CHOICES = blank_choice + tuple((x.id, x.name) for x in Events.objects.exclude(event_priority=0))
+SUBJECT_CHOICES = ((x.id, x.name) for x in Subjects.objects.all())
+PROFILE_CHOICES = ((x.id, x.name) for x in Profiles.objects.all())
 
 
 class DateInput(forms.DateInput):
     input_type = 'date'
 
+
 class EventForm(forms.Form):
-    name = forms.CharField(label='Название олимпиады')
-    notify_date = forms.DateField(label='Дата напоминалки')
-    grades = forms.ChoiceField(label='Выберите класс', widget=forms.CheckboxSelectMultiple)
+    name = forms.CharField(label='Название олимпиады', widget=forms.TextInput(attrs={'size':80}))
+    notify_date = forms.DateField(label='Дата напоминалки', widget=DateInput)
+    grades = forms.MultipleChoiceField(label='Выберите класс', widget=forms.CheckboxSelectMultiple,
+                                       choices=((1, 11), (2, 10), (3, 9), (4, 8)))
     period = forms.CharField(label='Сраки проведения')
     eventLevel = forms.ChoiceField(label='Уровень олимпиады', choices=((1, 1), (2, 2), (3, 3)))
     priority = forms.IntegerField(label='Отборочность олимпиады', min_value=0, max_value=3)
-    subject = forms.MultipleChoiceField(label='Предмет олимпиады')
-    profile = forms.MultipleChoiceField(label='Профиль олимпиады')
-    nextEvent = forms.ChoiceField(label='Следующий этап олимпиады')
-    event_url = forms.URLField(label='Ссылка на ивент')
-    description = forms.CharField(label='Описание', widget=forms.Textarea)
+    subject = forms.MultipleChoiceField(label='Предмет олимпиады', choices=SUBJECT_CHOICES)
+    profile = forms.MultipleChoiceField(label='Профиль олимпиады', choices=PROFILE_CHOICES)
+    nextEvent = forms.ChoiceField(label='Следующий этап олимпиады', required=False, choices=EVENT_CHOICES)
+    event_url = forms.URLField(label='Ссылка на ивент', required=False)
+    description = forms.CharField(label='Описание', widget=forms.Textarea, required=False)
--- eventHandler/views.py
+++ eventHandler/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 from .forms import EventForm
+from django.db.models.query import QuerySet
 from .models import Subjects, Events, Profiles
 
 
@@ -10,26 +11,14 @@ def test(request):
     if request.method == 'POST':
         eventForm = EventForm(request.POST)
         if eventForm.is_valid():
-            print('Fuck yeah')
-            return
+
+
             pass
         else:
-            print(eventForm.errors)
-            print('Fuck no')
+
             return HttpResponse(eventForm.errors)
             pass
-    class_choices = (('1', 11), ('2', 10), ('3', 9), ('4', 8))
+
     eventForm = EventForm()
-    eventForm.fields['grades'].choices = class_choices
-
-    subjects = Subjects.objects.all()
-    eventForm.fields['subject'].choices = ((x.id, x.name) for x in subjects)
-
-    profiles = Profiles.objects.all()
-    eventForm.fields['profile'].choices = ((x.id, x.name) for x in profiles)
-
-    blank_choice = (('', '--- Выберите значение ---'),)
-    events = Events.objects.exclude(event_priority=0)
-    eventForm.fields['nextEvent'].choices = blank_choice + tuple((x.id, x.name) for x in events)
 
     return render(request, 'test.html', {'form': eventForm})
