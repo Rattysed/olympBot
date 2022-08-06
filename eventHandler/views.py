83d59f7dc6a6c955a116e88c9b7625d14aba0008
Rattysed <ckvortcov71@gmail.com>
2022-08-06 09:59:29 +0300

    Добавил "расписание"

--- eventHandler/views.py
+++ eventHandler/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, HttpResponseRedirect, HttpResponse
 from .forms import EventForm
-from django.db.models.query import QuerySet
-from .models import Subjects, Events, Profiles
+from .models import Events
 
 
 def test(request):
@@ -11,8 +10,28 @@ def test(request):
     if request.method == 'POST':
         eventForm = EventForm(request.POST)
         if eventForm.is_valid():
-
-
+            for grade in eventForm.cleaned_data['grades']:
+                grade = int(grade)
+                event = Events()
+                event.name = eventForm.cleaned_data['name']
+                event.notify_date = eventForm.cleaned_data['notify_date']
+                event.period = eventForm.cleaned_data['period']
+                event.level = eventForm.cleaned_data['event_level']
+                event.event_priority = eventForm.cleaned_data['priority']
+                event.save()
+                for sub in eventForm.cleaned_data['subject']:
+                    event.subject.add(int(sub))
+                for prof in eventForm.cleaned_data['profile']:
+                    event.profile.add(int(prof))
+                event.event_grade = grade
+                try:
+                    event.next_event_id = int(eventForm.cleaned_data['next_event'])
+                except:
+                    print('Nope')
+                event.event_url = eventForm.cleaned_data['event_url']
+                event.description = eventForm.cleaned_data['description']
+                event.save()
+            return HttpResponse('Хуй соси')
             pass
         else:
 
